1e095fbdd0a24a8b7c23c45617dec7a5a11d6176
Slayred <aleksch1989@gmail.com>
2021-12-28 22:34:15 +0300

    complete with images

--- app/src/main/java/com/chibisov/todoapplication/fragments/list/ListFragment.kt
+++ app/src/main/java/com/chibisov/todoapplication/fragments/list/ListFragment.kt
@@ -12,11 +12,11 @@ import com.chibisov.todoapplication.R
 //private const val ARG_PARAM1 = "param1"
 //private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+///**
+// * A simple [Fragment] subclass.
+// * Use the [ListFragment.newInstance] factory method to
+// * create an instance of this fragment.
+// */
 class ListFragment : Fragment() {
 //    // TODO: Rename and change types of parameters
 //    private var param1: String? = null
