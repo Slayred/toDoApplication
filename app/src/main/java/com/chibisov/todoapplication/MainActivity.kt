19e0d19301509eb71cd5f5b787df88e800341bad
Slayred <aleksch1989@gmail.com>
2022-01-09 20:23:14 +0300

    2-8

--- app/src/main/java/com/chibisov/todoapplication/MainActivity.kt
+++ app/src/main/java/com/chibisov/todoapplication/MainActivity.kt
@@ -2,10 +2,26 @@ package com.chibisov.todoapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavHostController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //setupActionBarWithNavController(findNavController(R.id.navHostFragment))
+        val nabHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = nabHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
