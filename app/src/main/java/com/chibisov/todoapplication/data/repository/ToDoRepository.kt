b9ca7875ffae5fa0ebfcf098a0b2b764b5dbb4d1
Slayred <aleksch1989@gmail.com>
2022-01-16 15:10:02 +0300

    refactoring

--- app/src/main/java/com/chibisov/todoapplication/data/repository/ToDoRepository.kt
+++ app/src/main/java/com/chibisov/todoapplication/data/repository/ToDoRepository.kt
@@ -8,8 +8,7 @@ class ToDoRepository(private val toDoDao: ToDoDao) {
 
     val getAllData: LiveData<List<ToDoData>> = toDoDao.getAllData()
 
-//    val sortByHighPriority: LiveData<List<ToDoData>> = toDoDao.sortByHighPriority()
-//    val sortByLowPriority: LiveData<List<ToDoData>> = toDoDao.sortByLowPriority()
+
 
 
     suspend fun insertData(toDoData: ToDoData) {
